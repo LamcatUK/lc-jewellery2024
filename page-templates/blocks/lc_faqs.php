6a28c4ca4a46e9222a20662ae35ed794d75c904f
CheechRockwizard <136230172+LamcatUK@users.noreply.github.com>
2024-03-24 20:04:37 +0000

    faqs

--- page-templates/blocks/lc_faqs.php
+++ page-templates/blocks/lc_faqs.php
@@ -1,5 +1,8 @@
-<section class="faq_block py-5">
-    <div class="container-xl" data-aos="fade-up">
+<?php
+$classes = $block['className'] ?? 'py-5';
+?>
+<section class="faq_block <?=$classes?>">
+    <div class="container-xl" data-aos="fade">
         <?php
         if (get_field('faq_title')) {
             ?>
@@ -8,53 +11,34 @@
         </h2>
         <?php
         }
-        if (get_field('faq_intro')) {
-            ?>
+if (get_field('faq_intro')) {
+    ?>
         <div class="mb-4 faq_intro">
             <?=get_field('faq_intro')?>
         </div>
         <?php
-        }
-        ?>
+}
+?>
         <div class="faq_block__inner">
             <?php
-    $accordion = random_str(5);
-
-        echo '<div itemscope="" itemtype="https://schema.org/FAQPage" id="accordion' .  $accordion . '" class="accordion accordion-flush">';
-        $counter = 0;
-        $show = '';
-        $collapsed = 'collapsed';
-        while (have_rows('faqs')) {
-            the_row();
-            $ac = $accordion . '_' . $counter;
-            ?>
-            <div itemscope="" itemprop="mainEntity" itemtype="https://schema.org/Question" class="accordion-item">
-                <div class="accordion-head accordion-collapse <?=$collapsed?>"
-                    itemprop="name" data-bs-toggle="collapse"
-                    id="heading_<?=$ac?>"
-                    data-bs-target="#c<?=$ac?>" role="button"
-                    aria-expanded="true" aria-controls="c<?=$ac?>">
-                    <div class="pb-1">
-                        <?=get_sub_field('question')?>
-                    </div>
+echo '<div itemscope="" itemtype="https://schema.org/FAQPage">';
+$counter = 0;
+while (have_rows('faqs')) {
+    the_row();
+    ?>
+            <div itemscope="" itemprop="mainEntity" itemtype="https://schema.org/Question">
+                <div itemprop="name" class="faq__question">
+                    <?=get_sub_field('question')?>
                 </div>
-                <div class="collapse <?=$show?>" itemscope=""
-                    itemprop="acceptedAnswer" itemtype="https://schema.org/Answer"
-                    id="c<?=$ac?>"
-                    aria-labelledby="heading_<?=$ac?>"
-                    data-bs-parent="#accordion<?=$accordion?>">
-                    <div itemprop="text" class="faq__answer mb-4">
-                        <p><?=get_sub_field('answer')?>
-                        </p>
+                <div itemscope="" itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
+                    <div itemprop="text" class="faq__answer">
+                        <?=get_sub_field('answer')?>
                     </div>
                 </div>
             </div>
             <?php
-            $counter++;
-            $show = '';
-            $collapsed = 'collapsed';
-        }
-        echo '</div>';
-        ?>
+}
+echo '</div>';
+?>
         </div>
 </section>
