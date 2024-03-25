c7138cf1bfed69a2a63e8f170a6ef36ddd3c98e2
CheechRockwizard <136230172+LamcatUK@users.noreply.github.com>
2024-03-25 08:31:05 +0000

    Update lc_split.php

--- page-templates/blocks/lc_split.php
+++ page-templates/blocks/lc_split.php
@@ -4,7 +4,7 @@ $bg = get_field('colour') ?? null;
 
 $image_order = get_field('order') == 'Image/Text' ? '' : 'order-md-2';
 $text_order = get_field('order') == 'Image/Text' ? '' : 'order-md-1';
-$text_alignment = get_field('order') == 'Image/Text' ? 'text--right' : 'text--left';
+$text_alignment = get_field('order') == 'Image/Text' ? 'text--right' : 'text--left  ';
 
 $img = wp_get_attachment_image_url(get_field('image'), 'full') ?: get_stylesheet_directory_uri() . '/img/placeholder-800x450.png';
 ?>
@@ -30,7 +30,7 @@ $img = wp_get_attachment_image_url(get_field('image'), 'full') ?: get_stylesheet
                             $l = get_field('link');
                             ?>
                         <a href="<?=$l['url']?>"
-                            class="btn btn-primary"
+                            class="btn btn-primary mx-auto ms-md-0"
                             target="<?=$l['target']?>"><?=$l['title']?></a>
                         <?php
                         }
