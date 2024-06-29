51509efdea21c826b274d124db0824040cd44bff
LamcatUK <136230172+LamcatUK@users.noreply.github.com>
2024-06-29 13:21:09 +0100

    stuff

--- inc/lc-blocks.php
+++ inc/lc-blocks.php
@@ -93,6 +93,15 @@ function acf_blocks()
             'mode'	=> 'edit',
             'supports' => array('mode' => false),
         ));
+        acf_register_block_type(array(
+            'name'				=> 'lc_banner',
+            'title'				=> __('LC Banner'),
+            'category'			=> 'layout',
+            'icon'				=> 'cover-image',
+            'render_template'	=> 'page-templates/blocks/lc_banner.php',
+            'mode'	=> 'edit',
+            'supports' => array('mode' => false),
+        ));
     }
 }
 add_action('acf/init', 'acf_blocks');
--- page-templates/blocks/lc_four_images.php
+++ page-templates/blocks/lc_four_images.php
@@ -5,11 +5,12 @@ $img = get_field('columns')[0] == '4' ? '' : 'six_images';
     <?php
 $r = random_str(4);
 foreach (get_field('images') as $i) {
+    $caption = wp_get_attachment_caption($i) ?? null;
+    $alt = get_post_meta($i, '_wp_attachment_image_alt', true) ?? $caption;
     ?>
     <a href="<?=wp_get_attachment_image_url($i, 'full')?>"
-        class="glightbox" data-gallery="gallery<?=$r?>">
-        <img src="<?=wp_get_attachment_image_url($i, 'large')?>"
-            alt="">
+        class="glightbox" data-glightbox="title: <?=esc_html($caption)?>" data-gallery="gallery<?=$r?>">
+        <?=wp_get_attachment_image($i, 'large', false, array('alt' => esc_html($alt)))?>
     </a>
     <?php
 }
