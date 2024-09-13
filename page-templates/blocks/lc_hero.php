ac1f7d9b46d6c37cdf2a4ffe0d7e52e673a288cf
LamcatUK <136230172+LamcatUK@users.noreply.github.com>
2024-09-13 21:02:45 +0100

    Update lc_hero.php

--- page-templates/blocks/lc_hero.php
+++ page-templates/blocks/lc_hero.php
@@ -1,6 +1,6 @@
 
 <section class="hero">
-    <div class="container-xl">
+    <div class="container-xl px-0">
         <?=wp_get_attachment_image(get_field('background'), 'full', false, array('class' => 'hero__bg'))?>
     </div>
 </section>
