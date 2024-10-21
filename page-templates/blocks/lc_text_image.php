b10ad21e734cb2cc68c14f1a8266524ba772dfd7
LamcatUK <136230172+LamcatUK@users.noreply.github.com>
2024-10-21 19:46:55 +0100

    Update lc_text_image.php

--- page-templates/blocks/lc_text_image.php
+++ page-templates/blocks/lc_text_image.php
@@ -13,42 +13,42 @@ $imganim = 'fade';
     <div class="container-xl">
         <?php
         if (get_field('title') ?? null) {
-            ?>
-        <div class="h2 text-center d-md-none" data-aos="fade">
-            <?=get_field('title')?>
-        </div>
+        ?>
+            <div class="h2 text-center d-md-none" data-aos="fade">
+                <?= get_field('title') ?>
+            </div>
         <?php
         }
-?>
+        ?>
         <div class="row g-4">
-            <div class="col-md-6 <?=$txtcol?> d-flex flex-column justify-content-center"
-                data-aos="<?=$txtanim?>">
+            <div class="col-md-6 <?= $txtcol ?> d-flex flex-column justify-content-center"
+                data-aos="<?= $txtanim ?>">
                 <?php
-        if (get_field('title') ?? null) {
-            ?>
-                <h2 class="d-none d-md-block text-center h2">
-                    <?=get_field('title')?>
-                </h2>
+                if (get_field('title') ?? null) {
+                ?>
+                    <h2 class="d-none d-md-block text-center h2">
+                        <?= get_field('title') ?>
+                    </h2>
                 <?php
-        }
-?>
-                <div><?=get_field('content')?>
+                }
+                ?>
+                <div><?= get_field('content') ?>
                 </div>
                 <?php
-if (get_field('link') ?? null) {
-    $l = get_field('link');
-    ?>
-                <a href="<?=$l['url']?>"
-                    target="<?=$l['target']?>"
-                    class="mt-4 btn btn-red text-center align-self-center align-self-md-start"><?=$l['title']?></a>
+                if (get_field('link') ?? null) {
+                    $l = get_field('link');
+                ?>
+                    <a href="<?= $l['url'] ?>"
+                        target="<?= $l['target'] ?>"
+                        class="btn btn-primary mx-auto mt-3"><?= $l['title'] ?></a>
                 <?php
-}
-?>
+                }
+                ?>
             </div>
-            <div class="col-md-6 <?=$imgcol?> d-flex align-items-center"
-                data-aos="<?=$imganim?>">
-                <img src="<?=wp_get_attachment_image_url(get_field('image'), 'large')?>"
-                    alt="<?=get_field('title')?>"
+            <div class="col-md-6 <?= $imgcol ?> d-flex align-items-center"
+                data-aos="<?= $imganim ?>">
+                <img src="<?= wp_get_attachment_image_url(get_field('image'), 'large') ?>"
+                    alt="<?= get_field('title') ?>"
                     class="feature__img mx-auto">
             </div>
         </div>
