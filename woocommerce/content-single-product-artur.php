575d4661e50e14d6e74aa41701b93683f0ef4ad0
LamcatUK <git@lamcat.co.uk>
2024-11-03 18:08:31 +0000

    indicators

--- woocommerce/content-single-product-artur.php
+++ woocommerce/content-single-product-artur.php
@@ -76,6 +76,14 @@ $img = get_the_post_thumbnail_url(get_the_ID(), 'full');
                         }
                         ?>
                     </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carousel" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carousel" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
                 </div>
             </div>
             <div class="col-lg-6 watch__content d-flex align-items-center bg--grey-200">
