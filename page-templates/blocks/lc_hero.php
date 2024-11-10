1298d33df251fe963bd0a4c5b90a30bc83597888
LamcatUK <136230172+LamcatUK@users.noreply.github.com>
2024-11-10 16:47:58 +0000

    hero align and mobile height

--- page-templates/blocks/lc_hero.php
+++ page-templates/blocks/lc_hero.php
@@ -1,6 +1,9 @@
-
+<?php
+$align = get_field('align') ?? null;
+$class = 'hero__bg ' . $align;
+?>
 <section class="hero">
     <div class="container-xl px-0">
-        <?=wp_get_attachment_image(get_field('background'), 'full', false, array('class' => 'hero__bg'))?>
+        <?= wp_get_attachment_image(get_field('background'), 'full', false, array('class' => $class)) ?>
     </div>
 </section>
