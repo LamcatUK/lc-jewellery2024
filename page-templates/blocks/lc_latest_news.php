a9e8f36c6294eda055e6e3e579d55058cb0a901a
LamcatUK <git@lamcat.co.uk>
2025-03-15 17:46:08 +0000

    insights fade

--- page-templates/blocks/lc_latest_news.php
+++ page-templates/blocks/lc_latest_news.php
@@ -8,14 +8,14 @@ if ($q->have_posts()) {
 ?>
     <section class="latest py-5">
         <div class="container-xl py-4 px-0">
-            <div class="full_text mb-4">
+            <div class="full_text mb-4" data-aos="fade">
                 <div class="full_text__pre_title">
                     The Art & Science of Fine Craftsmanship
                 </div>
                 <h1 class="full_text__title">
                     From the jeweller's bench
                 </h1>
-                <p class="text-center max-ch mx-auto" data-aos="fade">At Griffin & Sloane, craftsmanship extends beyond creation—it is a story of artistry, heritage, and expertise. Our Latest Insights invite you to explore the intricacies of fine jewellery, the evolution of horology, and the timeless allure of the world's most extraordinary gemstones.</p>
+                <p class="text-center max-ch mx-auto" data-aos="fade">At Griffin & Sloane, craftsmanship extends beyond creation—it is a story of artistry, heritage, and expertise. Our insights invite you to explore the intricacies of fine jewellery, the evolution of horology, and the timeless allure of the world's most extraordinary gemstones.</p>
             </div>
             <div class="news_index__grid pb-5">
                 <?php
