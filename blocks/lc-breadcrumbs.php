393b56bc99ae99b6d72d8911013e36067a4755b4
LamcatUK <git@lamcat.co.uk>
2026-01-30 14:56:04 +0000

    bread

--- blocks/lc-breadcrumbs.php
+++ blocks/lc-breadcrumbs.php
@@ -1,9 +1,20 @@
+<?php
+/**
+ * Block Name: LC Breadcrumbs
+ *
+ * This is the template that displays the LC Breadcrumbs block.
+ *
+ * @package lc-jewellery2024
+ */
+
+defined( 'ABSPATH' ) || exit;
+?>
 <section class="breadcrumbs">
-    <div class="container-xl text-center">
-        <?php
-        if (function_exists('yoast_breadcrumb')) {
-            yoast_breadcrumb('<div id="breadcrumbs" class="mt-2">', '</div>');
-        }
-        ?>
-    </div>
+	<div class="container-xl text-center">
+		<?php
+		if ( function_exists( 'yoast_breadcrumb' ) ) {
+			yoast_breadcrumb('<div id="breadcrumbs" class="mt-4">', '</div>');
+		}
+		?>
+	</div>
 </section>
